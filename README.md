f3e1902de64e8a611845c1b5b26263a30f7e2243
root <1121114685@qq.com>
2020-02-21 20:53:11 +0800

    刚做完，推送上去测试

--- README.md
+++ README.md
@@ -1,17 +1,34 @@
 ## Superspeed
 Superspeed.sh with updated server lists.
 
-## Usage
-```
-bash <(curl -Lso- https://git.io/superspeed)
-```
+## speedtest-cli 官方地址
+`https://www.speedtest.net/apps/cli`本脚从superspeed中修改而来，其实是我太懒，不愿意多折腾superspeed作者写的挺好的，就拿过来了。
 
-[查看全部节点列表](https://git.io/superspeedList)
+## Usage 用法
+--- 
+` wget -N --no-check-certificate "https://raw.githubusercontent.com/user1121114685/speedtest_cli/master/spd.sh" && chmod +x spd.sh && bash spd.sh`
 
-![测速图](https://i.loli.net/2019/12/23/H8WtjGTgDqVsUaL.jpg)
+---
+## 再次使用
+
+`bash spd.sh`
+
+#### 欢迎各位大佬指点，和修正，小白复制拷贝的。
+
+
+![目前版本截图](https://github.com/user1121114685/baidupcsweb/blob/master/20181203230829.png?raw=true "后续更新可能还会有更新！")
+
 
 ---
 
-Modified from Oldking's script. Original Info:
-- Description: Test your server's network with Speedtest to China
-- Copyright (C) 2018 Oldking <oooldking@gmail.com>
+---
+## 如果对你有所帮助，也可以对我进行捐赠。那撒我也不废话，下面是二维码。
+[微信](https://gitee.com/shaoxia1991/Blog/raw/master/me/%E5%BE%AE%E4%BF%A1%E6%94%B6%E6%AC%BE.png)  
+
+[支付宝](https://gitee.com/shaoxia1991/Blog/raw/master/me/%E6%94%AF%E4%BB%98%E5%AE%9D%E6%94%B6%E6%AC%BE.jpg)  
+
+---
+
+## License
+
+[The MIT License (MIT)](https://raw.githubusercontent.com/user1121114685/google_to_wechat/master/LICENSE)
---
+++ spd.sh
@@ -0,0 +1,218 @@
+#!/usr/bin/env bash
+
+# Colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE="\033[0;35m"
+CYAN='\033[0;36m'
+PLAIN='\033[0m'
+
+
+#  版本信息 用于更新脚本
+SH_VER="1.0"
+
+# check root
+[[ $EUID -ne 0 ]] && echo -e "${RED}Error:${PLAIN} 请用root权限运行脚本！" && exit 1
+
+#  https://www.speedtest.net/speedtest-servers-static.php
+
+# check wget
+if  [ ! -e '/usr/bin/wget' ]; then
+	if [ "${release}" == "centos" ]; then
+			yum -y install wget
+	else
+			apt-get -y install wget
+	fi
+fi
+
+
+# check curl
+if  [ ! -e '/usr/bin/curl' ]; then
+	if [ "${release}" == "centos" ]; then
+			yum -y install curl
+	else
+			apt-get -y install curl
+	fi
+fi
+
+# 检查节点节点
+check_speedtest_servers(){
+	# 下载所有节点信息
+	curl  https://www.speedtest.net/speedtest-servers-static.php > /tmp/spd_cli/1.txt
+	# 将国内节点剔出来
+	cat /tmp/spd_cli/1.txt | grep cc=\"CN\" | grep -v "Hong Kong" > /tmp/spd_cli/2.txt
+	# 提取 节点ID
+	cat /tmp/spd_cli/2.txt | awk -F "=" '{print $9}' | awk -F "\"" '{print $2}'  > /tmp/spd_cli/3.txt
+	# 提取节点地区
+	cat /tmp/spd_cli/2.txt | awk -F "=" '{print $5}' | awk -F "\"" '{print $2}'  > /tmp/spd_cli/4.txt
+	# 提取 节点运营商 
+	cat /tmp/spd_cli/2.txt | awk -F "=" '{print $8}' | awk -F "\"" '{print $2}'  > /tmp/spd_cli/5.txt
+}
+
+if  [ ! -e '/tmp/spd_cli/1.txt' ]; then
+	check_speedtest_servers
+fi
+
+clear
+
+echo "————————————————————————speedtest-cli测试端———————————           —————————————————————"
+echo "     项目地址:      https://github.com/user1121114685/speedtest_cli"
+echo "     原脚本地址：https://github.com/ernisn/superspeed"
+echo "     懒人专用，推荐在晚上21:30至凌晨1:00之间测试，高峰期更具有实际意义。"
+echo "——————————————————————————————————————————————————————————————————————"
+echo "     选择测速类型: "
+echo -e "     ${GREEN}1.${PLAIN} 随机5个国内节点测试       ${GREEN}2.${PLAIN} 随机10个国内节点测试       ${GREEN}3.${PLAIN} 指定单个测试节点     "
+echo -e "     ${GREEN}4.${PLAIN} 升级脚本                               ${GREEN}5.${PLAIN} 更新节点信息                         ${GREEN}6.${PLAIN} 展示所有节点"
+
+while :; do echo
+		read -p "     请输入数字选择: " selection
+		if [[ ! $selection =~ ^[1-6]$ ]]; then
+				echo -ne "     ${RED}输入错误${PLAIN}, 请输入正确的数字!"
+		else
+				break   
+		fi
+done
+
+# 判断 系统 运行位数
+	bit=`uname -m`
+	if [[ ${bit} == "x86_64" ]]; then
+		bit="x86_64"
+	elif [[ ${bit} == "i386" || ${bit} == "i686" ]]; then
+		bit="i386"
+
+	fi
+
+
+# install speedtest
+if  [ ! -e '/tmp/spd_cli/speedtest' ]; then
+	wget --no-check-certificate -P tmp/spd_cli  https://raw.githubusercontent.com/user1121114685/speedtest_cli/master/spd_cli/${bit}/speedtest  > /dev/null 2>&1
+fi
+chmod a+rx /tmp/spd_cli/speedtest
+
+speed_test(){
+	/tmp/spd_cli/speedtest -s $1
+	echo "——————————————————————————————————————————————————————————————————————"
+
+}
+
+# 5个节点测试
+if [[ ${selection} == 1 ]]; then
+	echo "——————————————————————————————————————————————————————————————————————"
+		speedtest_cli_id=`cat /tmp/spd_cli/3.txt | shuf -n5`
+
+	start=$(date +%s) 
+
+	for ((i=0;i<6;i++))
+		{ 
+			speed_test speedtest_cli_id[$i]
+		}
+	end=$(date +%s)  
+
+	echo "——————————————————————————————————————————————————————————————————————"
+	time=$(( $end - $start ))
+	if [[ $time -gt 60 ]]; then
+		min=$(expr $time / 60)
+		sec=$(expr $time % 60)
+		echo -ne "     测试完成, 本次测速耗时: ${min} 分 ${sec} 秒"
+	else
+		echo -ne "     测试完成, 本次测速耗时: ${time} 秒"
+	fi
+	echo -ne "\n     当前时间: "
+	echo $(date +%Y-%m-%d" "%H:%M:%S)
+fi
+
+# 10 个节点测试
+if [[ ${selection} == 2 ]]; then
+	echo "——————————————————————————————————————————————————————————————————————"
+		speedtest_cli_id=`cat /tmp/spd_cli/3.txt | shuf -n10`
+
+	start=$(date +%s) 
+
+	for ((i=0;i<11;i++))
+		{ 
+			speed_test speedtest_cli_id[$i]
+		}
+	end=$(date +%s)  
+
+	echo "——————————————————————————————————————————————————————————————————————"
+	time=$(( $end - $start ))
+	if [[ $time -gt 60 ]]; then
+		min=$(expr $time / 60)
+		sec=$(expr $time % 60)
+		echo -ne "     测试完成, 本次测速耗时: ${min} 分 ${sec} 秒"
+	else
+		echo -ne "     测试完成, 本次测速耗时: ${time} 秒"
+	fi
+	echo -ne "\n     当前时间: "
+	echo $(date +%Y-%m-%d" "%H:%M:%S)
+fi
+
+
+# 单个节点测试
+if [[ ${selection} == 3 ]]; then
+	echo "——————————————————————————————————————————————————————————————————————"
+	read -p "     请输入一个节点ID: " selection
+
+	start=$(date +%s) 
+
+
+	speed_test speedtest_cli_id[$selection]
+
+	end=$(date +%s)  
+
+	echo "——————————————————————————————————————————————————————————————————————"
+	time=$(( $end - $start ))
+	if [[ $time -gt 60 ]]; then
+		min=$(expr $time / 60)
+		sec=$(expr $time % 60)
+		echo -ne "     测试完成, 本次测速耗时: ${min} 分 ${sec} 秒"
+	else
+		echo -ne "     测试完成, 本次测速耗时: ${time} 秒"
+	fi
+	echo -ne "\n     当前时间: "
+	echo $(date +%Y-%m-%d" "%H:%M:%S)
+
+fi
+
+#  脚本更新
+if [[ ${selection} == 4 ]]; then
+
+	latest_version=$(curl -H 'Cache-Control: no-cache' -s -L "https://raw.githubusercontent.com/user1121114685/speedtest_cli/master/spd.sh" | grep 'SH_VER' -m1 | cut -d\" -f2)
+	if [[ ! $latest_version ]]; then
+		echo
+		echo -e " ${RED}获取SpeedTest_Cli测试脚本 最新版本失败!!!${PLAIN} "
+		echo
+		echo -e " 请检查网络配置！"
+		echo
+		echo " 然后再继续...."
+		echo
+		exit 1
+	fi
+
+	if [[ $latest_version == $SH_VER ]]; then
+		echo
+		echo -e "${GREEN} 木有发现新版本 ${PLAIN} "
+		echo
+	else
+		echo
+		echo -e " ${GREEN} 咦...发现新版本耶....正在拼命更新.......${PLAIN} "
+		echo
+		wget -N --no-check-certificate "https://raw.githubusercontent.com/user1121114685/speedtest_cli/master/spd.sh" && chmod +x spd.sh
+		echo -e "脚本已更新为最新版本[ ${latest_version} ] !(注意：因为更新方式为直接覆盖当前运行的脚本，所以可能下面会提示一些报错，无视即可)" && exit 0
+	fi
+
+	
+fi
+
+#  手动更新节点信息
+if [[ ${selection} == 5 ]]; then
+	check_speedtest_servers
+
+fi
+
+# 展示所有的国内节点
+if [[ ${selection} == 6 ]]; then
+	paste -d" ----" /tmp/spd_cli/3.txt /tmp/spd_cli/4.txt /tmp/spd_cli/5.txt
+fi
